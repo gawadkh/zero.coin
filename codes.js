a4c17be53a4f719accae25021fd25303a15b010f
H.O.S <gawadkh319@hotmail.com>
2025-08-24 12:08:39 +0300

    Update codes.js

--- codes.js
+++ codes.js
@@ -1,1 +1,10 @@
-const allowedCodes = ["666001", "666002", "666003"];
+function checkCode() {
+  const allowedCodes = ["666001", "666002", "666003"];
+  const enteredCode = document.getElementById("codeInput").value.trim();
+
+  if (allowedCodes.includes(enteredCode)) {
+    window.location.href = "page4.html";
+  } else {
+    alert("⚠️ الكود غير صحيح");
+  }
+}
